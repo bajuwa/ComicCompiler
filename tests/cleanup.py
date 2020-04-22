2bc3a7548544d2910e7f8c625d0bb82cc49a0e01
bajuwa <justcallmebaj@gmail.com>
2020-04-22 14:27:06 -0700

    Refactor : Add "/Compiled/" folders within the project structure to the list of folders to remove in cleanup.py

--- tests/cleanup.py
+++ tests/cleanup.py
@@ -5,3 +5,6 @@ if __name__ == '__main__':
     for directory in glob.glob("**/*-test/", recursive=True):
         print("deleting: " + directory)
         shutil.rmtree(directory)
+    for directory in glob.glob("**/Compiled/", recursive=True):
+        print("deleting: " + directory)
+        shutil.rmtree(directory)
