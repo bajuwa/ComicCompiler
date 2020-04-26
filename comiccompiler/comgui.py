0cbe7d4265794aa448e0a412853ac4fd7c9f447c
bajuwa <justcallmebaj@gmail.com>
2020-04-26 09:35:40 -0700

    Resolves #88 : Fix the logic for finding the resource folder so that both the exe and the cli launcher can find the logo

--- comiccompiler/comgui.py
+++ comiccompiler/comgui.py
@@ -24,7 +24,9 @@ thread_pool_executor = futures.ThreadPoolExecutor(max_workers=1)
 def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath("."), relative_path)
+    if os.path.exists(relative_path):
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)), relative_path)
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "../" + relative_path)
 
 
 def input_list(entry, items):
