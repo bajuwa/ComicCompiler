39b3eb85e19c222f77a03d326beb2f09aec06a21
bajuwa <justcallmebaj@gmail.com>
2020-04-28 20:55:41 -0700

    Refactor : Fix unit tests to cover new 'unset breakpoint mode' tweaks

--- tests/min-page-height/test_min_height.py
+++ tests/min-page-height/test_min_height.py
@@ -7,12 +7,19 @@ from comiccompiler import compiler
 class MinHeightTests(tests.ComicomTestCase):
     def test_default_args(self):
         self.setup_test_vars("min-page-height", "Compiled-default")
-        self.args.min_height_per_page = 300
+        self.args.min_height_per_page = 400
         compiler.run(self.args)
         self.compare_output()
 
-    def test_breakpoint_unset(self):
+    def test_breakpoint_unset_default(self):
         self.setup_test_vars("min-page-height", "Compiled-default")
+        self.args.min_height_per_page = 400
+        self.args.breakpoint_detection_mode = -1
+        compiler.run(self.args)
+        self.compare_output()
+
+    def test_breakpoint_unset_dynamic(self):
+        self.setup_test_vars("min-page-height", "Compiled-b1")
         self.args.min_height_per_page = 300
         self.args.breakpoint_detection_mode = -1
         compiler.run(self.args)
